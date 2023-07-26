1f772803f6d4accdb61b13ea109b333caaed4460
gluk2309 <gluk2309@mail.ru>
2023-07-26 14:45:37 +0300

    Formula editing

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         CreditPaymentService service = new CreditPaymentService();
 
         int numberOfMonths = 12;
-        double interestRate = 9.99;
+        double interestRate = 9.99 / 12;
         int amountOfCredit = 1_000_000;
 
         int credit = service.calculate(numberOfMonths,interestRate, amountOfCredit);
