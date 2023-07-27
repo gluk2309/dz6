c3ed4982980c02ff96bb03c440906e23f8f1ae97
gluk2309 <gluk2309@mail.ru>
2023-07-27 12:01:30 +0300

    Final work

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,7 +1,7 @@
 public class CreditPaymentService {
     public int calculate(int numberOfMonths, double interestRate, int amountOfCredit) {
 
-        double result = amountOfCredit * interestRate * Math.pow(1 + interestRate, numberOfMonths) / (Math.pow(1 + interestRate, numberOfMonths) - 1);
+        double result = amountOfCredit * (interestRate / 12 / 100) * Math.pow(1 + (interestRate / 12 / 100), numberOfMonths) / (Math.pow(1 + (interestRate / 12 / 100), numberOfMonths) - 1);
 
         return (int) result;
     }
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,20 @@
 public class Main {
     public static void main(String[] args) {
 
+        //int numberOfMonths = ;
+        //double interestRate = 9.99;
+        //int amountOfCredit = 1_000_000;
+
         CreditPaymentService service = new CreditPaymentService();
+        int credit = service.calculate(12, 9.99, 1_000_000);
+        System.out.println("Ежемесячный платёж на 12 месяцев - " + credit);
 
-        int numberOfMonths = 12;
-        double interestRate = 9.99 / 12;
-        int amountOfCredit = 1_000_000;
 
-        int credit = service.calculate(numberOfMonths,interestRate, amountOfCredit);
+        int credit2 = service.calculate(24, 9.99, 1_000_000);
+        System.out.println("Ежемесячный платёж на 24 месяцев - " + credit2);
 
-        System.out.println(credit);
+
+        int credit3 = service.calculate(36, 9.99, 1_000_000);
+        System.out.println("Ежемесячный платёж на 36 месяцев - " + credit3);
     }
 }
